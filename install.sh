49255c2ec5ccec8463140470b3e9ffb518c52c4f
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-20 20:11:46 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ echo "system>   1) Linux x86(32-bit system)"
 echo "system>   2) Linux x64(64-bit system)"
 echo "system>   3) Mac x86(32-bit system)"
 echo "system>   4) Mac x64(64-bit system)"
-read -p "system> Number (e.g. 1): "
-sleep 10
+read -p "system> Number (e.g. 1): " nothing
+# sleep 10
 read a
 
 case "$a" in 
