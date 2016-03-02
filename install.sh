1708d43a8f8b9d799ec37f2807cc0d8a1d36fe14
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-03-02 18:44:54 +1100

    -_-

--- install.sh
+++ install.sh
@@ -102,70 +102,7 @@ fi
 		fi
         
         echo
-        read -e -p "system> ImagicalMine installation completed! Do you want to start ImagicalMine now? (Y/n):" c
-        read -e -p "system> ImagicalMine installation completed! Do you want to start ImagicalMine now? (Y/n):" c </dev/tty
-#		read c
-		if [ "$c" == "y" ]||[ "$c" == "Y" ]; then
-                DO_LOOP="no"
-
-while getopts "p:f:l" OPTION 2> /dev/null; do
-	case ${OPTION} in
-		p)
-			PHP_BINARY="$OPTARG"
-			;;
-		f)
-			POCKETMINE_FILE="$OPTARG"
-			;;
-		l)
-			DO_LOOP="yes"
-			;;
-		\?)
-			break
-			;;
-	esac
-done
-
-if [ "$PHP_BINARY" == "" ]; then
-	if [ -f ./bin/php7/bin/php ]; then
-		export PHPRC=""
-		PHP_BINARY="./bin/php7/bin/php"
-	elif [ type php 2>/dev/null ]; then
-		PHP_BINARY=$(type -p php)
-	else
-		echo "Couldn't find a working PHP binary, please use the installer."
-		exit 7
-	fi
-fi
-
-if [ "$POCKETMINE_FILE" == "" ]; then
-	if [ -f ./ImagicalMine.phar ]; then
-		POCKETMINE_FILE="./ImagicalMine.phar"
-	elif [ -f ./src/pocketmine/PocketMine.php ]; then
-		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
-	else
-		echo "Couldn't find a valid ImagicalMine installation. If you have recently upgraded, ensure that you have renamed PocketMine-MP.phar to ImagicalMine.phar"
-		exit 7
-	fi
-fi
-
-LOOPS=0
-
-set +e
-while [ "$LOOPS" -eq 0 ] || [ "$DO_LOOP" == "yes" ]; do
-	if [ "$DO_LOOP" == "yes" ]; then
-		"$PHP_BINARY" "$POCKETMINE_FILE" $@
-	else
-		exec "$PHP_BINARY" "$POCKETMINE_FILE" $@
-	fi
-	((LOOPS++))
-done
-
-if [ ${LOOPS} -gt 1 ]; then
-	echo "Restarted $LOOPS times"
-fi
-		else
-	        echo "system> ImagicalMine installation completed! You have decided to run ImagicalMine later. Run ./start.sh (or ./st*) to start ImagicalMine later."
-                fi
+        echo "system> ImagicalMine installation completed! Run ./start.sh (or ./st*) to start ImagicalMine."
 #        shopt -u extglob
 # fi
 exit 0
