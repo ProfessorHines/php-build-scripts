6a3acbf7e7b8899c7cdf093e363f381fdc777bde
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-03-04 12:34:30 +0300

    Swedish Language!

---
+++ language/sw.sh
@@ -0,0 +1,37 @@
+#! / Bin / bash
+
+# Detta kan användas för felsökning
+message_hello_world = "Hello world"
+
+# Globala meddelanden (visas flera gånger under)
+exit = "systemet> 4) Avsluta ImagicalMine installation"
+exit1 = "systemet> 6) Avsluta ImagicalMine installation"
+no_selection = "systemet> Number (t ex 1):"
+error_selection = "error> Ett oväntat fel uppstod - du har angett en okänd val Starta manus, och välj sedan igen.".
+
+# Enskilda meddelanden (endast visas en gång)
+welcome_to_imagicalmine = "systemet> Välkommen till ImagicalMine!"
+welcome_install_server = "systemet> Installationsprogrammet kommer att guida dig genom att installera ImagicalMine för din server!"
+
+language_translation = "systemet> Om ditt språk inte finns med i listan nedan, gärna punga de ImagicalMine / php-build-skript förvaret på GitHub och översätta det för oss, sedan göra en begäran dra så här installationsprogrammet innehåller ditt språk."
+language_selection = "systemet> Välj vilket språk du vill använda under installationen:"
+sv = "systemet> 1) English"
+ch = "systemet> 2) Kinesiska"
+de = "systemet> 3) Tyska"
+ru = "systemet> 4) Ryska
+sw = "systemet> 5) Svenska
+
+language_selected = "systemet> Du har valt engelska för det språk som ska användas under installation ImagicalMine."
+php_prompt = "systemet> Välj vilken PHP binär du vill installera:"
+linux_32 = "systemet> 1) Linux x86 (32-bitars)"
+linux_64 = "systemet> 2) Linux x64 (64-bitars)"
+mac_32 = "systemet> 3) Mac x86 (32-bitars)"
+mac_64 = "systemet> 4) Mac x64 (64-bitars)"
+rpi2 = "systemet> 5) Raspberry Pi 2"
+
+im_install_echo = "systemet> Installera ImagicalMine ..."
+php_install_echo = "systemet> Installera PHP binär ..."
+loop_prompt = "systemet> Vill du ImagicalMine att automatiskt starta om servern om det stannar eller kraschar?"
+ja = "systemet> 1) Ja"
+nej = "systemet> 2) Nej"
+installation_complete = "systemet> ImagicalMine installationen klar! Kör ./start.sh (eller ./st*) för att starta ImagicalMine."
