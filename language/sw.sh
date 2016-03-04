2ef8683ad23126106a757f70ad4d775387901778
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-03-04 21:27:10 +1100

    Some edits

--- language/sw.sh
+++ language/sw.sh
@@ -4,8 +4,8 @@
 message_hello_world = "Hello world"
 
 # Globala meddelanden (visas flera gånger under)
-exit = "systemet> 4) Avsluta ImagicalMine installation"
-exit1 = "systemet> 6) Avsluta ImagicalMine installation"
+exit = "systemet>   4) Avsluta ImagicalMine installation"
+exit1 = "systemet>   6) Avsluta ImagicalMine installation"
 no_selection = "systemet> Number (t ex 1):"
 error_selection = "error> Ett oväntat fel uppstod - du har angett en okänd val Starta manus, och välj sedan igen.".
 
@@ -13,15 +13,15 @@ error_selection = "error> Ett oväntat fel uppstod - du har angett en okänd val
 
 language_selected = "systemet> Du har valt engelska för det språk som ska användas under installation ImagicalMine."
 php_prompt = "systemet> Välj vilken PHP binär du vill installera:"
-linux_32 = "systemet> 1) Linux x86 (32-bitars)"
-linux_64 = "systemet> 2) Linux x64 (64-bitars)"
-mac_32 = "systemet> 3) Mac x86 (32-bitars)"
-mac_64 = "systemet> 4) Mac x64 (64-bitars)"
-rpi2 = "systemet> 5) Raspberry Pi 2"
+linux_32 = "systemet>   1) Linux x86 (32-bitars)"
+linux_64 = "systemet>   2) Linux x64 (64-bitars)"
+mac_32 = "systemet>   3) Mac x86 (32-bitars)"
+mac_64 = "systemet>   4) Mac x64 (64-bitars)"
+rpi2 = "systemet>   5) Raspberry Pi 2"
 
-im_install_echo = "systemet> Installera ImagicalMine ..."
-php_install_echo = "systemet> Installera PHP binär ..."
+im_install_echo = "systemet> Installera ImagicalMine..."
+php_install_echo = "systemet> Installera PHP binär..."
 loop_prompt = "systemet> Vill du ImagicalMine att automatiskt starta om servern om det stannar eller kraschar?"
-ja = "systemet> 1) Ja"
-nej = "systemet> 2) Nej"
+ja = "systemet>   1) Ja"
+nej = "systemet>   2) Nej"
 installation_complete = "systemet> ImagicalMine installationen klar! Kör ./start.sh (eller ./st*) för att starta ImagicalMine."
